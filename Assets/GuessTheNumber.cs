e5c84e640d4924270774232f269b0614b3041952
internetseekho <internetseekho.com@gmail.com>
2017-05-13 20:17:55 +0500

    Code you can use in tutorial 12 and 13
    Code you can use in tutorial 12 and 13

--- Assets/GuessTheNumber.cs
+++ Assets/GuessTheNumber.cs
@@ -4,40 +4,61 @@ using UnityEngine;
 
 public class GuessTheNumber : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
+    int Higherlimit = 100;
+    int Lowerlimit = 1;
+    int ComputerGuess;
+
+    // Use this for initialization
+    void Start () {
 
         print("Welcome to Guess The Number Game");
         print("Guess a Number in your Head and don't Tell me about it");
-
-        int Higherlimit = 100;
-        int Lowerlimit = 1;
-        int ComputerGuess = (Higherlimit + Lowerlimit) / 2;
+        // 100 + 1 / 2 float round 50
+        ComputerGuess = (Higherlimit + Lowerlimit) / 2;
 
         print("The Higher Limit you can think of is " + Higherlimit);
         print("The Lower Limit you can think of is "  + Lowerlimit);
 
         print("Is Your Number Lower or Higer Than " + ComputerGuess);
         print("For Higer Press Up Arrow, For Lower Press Down Arrow and For Equal Press Return/Enter");
-
+        Higherlimit = Higherlimit + 1;
 
     }
 	
 	// Update is called once per frame
 	void Update () {
 
-        if ( Input.GetKeyDown( KeyCode.UpArrow ) ) { 
-            print("Up Arrow Pressed");
-        }
-
-        if (Input.GetKeyDown(KeyCode.DownArrow))
+        if (Input.GetKeyDown(KeyCode.UpArrow))
         {
-            print("Down Arrow Pressed");
-        }
 
-        if (Input.GetKeyDown(KeyCode.Return)) { 
+            Lowerlimit = ComputerGuess;
+
+            NextComputerGuess();
+
+
+
+        }
+        else if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+
+            Higherlimit = ComputerGuess;
+            NextComputerGuess();
+
+
+        }
+        else if (Input.GetKeyDown(KeyCode.Return))
+        {
             print("Return/Enter Arrow Pressed");
         }
 
     }
+
+    void NextComputerGuess() {
+
+        ComputerGuess = (Higherlimit + Lowerlimit) / 2;
+        print("Is Your Number Lower or Higer Than " + ComputerGuess);
+        print("For Higer Press Up Arrow, For Lower Press Down Arrow and For Equal Press Return/Enter");
+
+    }
+
 }
